2524926979992e6d8f9c3aacfe0e0e14ceead0d9
Deibid1 <giraldo2035@gmail.com>
2025-09-16 11:01:42 -0500

    Create Seleccionador.py
    Seleccionador actualizado
    - desde el main enviaremos n hacia el seleccionador y ya

---
+++ builders/Seleccionador.py
@@ -0,0 +1,25 @@
+from builders.even_builder import EvenMagicSquareBuilder
+from builders.odd_builder import OddMagicSquareBuilder
+class Seleccionador:
+
+
+    def seleccionar(self, n):
+        if n % 2 == 1:
+            # Método para impares
+            odd = OddMagicSquareBuilder()
+            return odd.build(n)
+        
+        elif n % 4 == 0:
+            # Método para pares dobles (n divisible entre 4)
+            even = EvenMagicSquareBuilder()
+            return even.build(n)
+        
+        elif n % 2 == 0 and n % 4 != 0:
+            # Método para pares simples (los verdes)
+            green = EvenMagicSquareBuilder()
+            return green.build(n)
+        
+        else:
+            raise ValueError("El tamaño ingresado no es válido para construir un cuadrado mágico")
+
+    
